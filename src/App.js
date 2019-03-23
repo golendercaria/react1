6f3b55dc76cedf0ff648a078e6346b4cc444b026
golendercaria <yannvangampelaere@gmail.com>
2019-03-23 07:15:35 +0100

    Create little component for start formation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Membre from './components/Membre.js'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+	  return (
+		<Fragment>
+			<div className="App">
+				<header className="gol-App-header">
+					<h1>HELLO</h1>
+				</header>
+			  </div>
+			  <hr />
+			  <Membre />
+	  </Fragment>
     );
   }
 }
---
+++ src/components/Membre.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Membre = () => { 
+	return (
+		<ul>
+			<li>Yann</li>
+			<li>Marina</li>
+			<li>Gregory</li>
+		</ul>
+	)
+}
+
+export default Membre
